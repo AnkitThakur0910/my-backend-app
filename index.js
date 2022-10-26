e8f5e03ce6844bb0d20ffc9e25d96ecd3b08c8aa
ankit0910 <ankit9971389027@gmail.com>
2022-10-26 22:54:43 +0530

    aa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config;
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send({message:"dekh ansh bhusdk"});
+    res.send("dekh ansh bhusdk");
 })
 
 app.listen(process.env.port||8080);
